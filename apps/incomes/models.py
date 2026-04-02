e300ecb548e60ebe438b24e5d1aa7c08f5d974e4
Ramses Solano Ramirez <ramsessr@outlook.com>
2026-04-02 09:45:52 -0500

    incomes integration

--- apps/incomes/models.py
+++ apps/incomes/models.py
@@ -1,14 +1,65 @@
-from django import models
+from django.db import models
 
-class Income(models.Model): 
-    
+class Income(models.Model):
     id = models.IntegerField(primary_key=True)
-    description = models.CharField( db_column="descripcion" , max_length=250)
-    amount = models.DecimalField( db_column="monto", max_digits=5, decimal_places=2)
-    category = models.CharField( db_column="categoria", max_length=100)
-    incomeDate = models.DateField( db_column="fecha")
-    userId = models.IntegerField( db_column="user_id" )
-    
+
+    value = models.DecimalField(
+        max_digits=14,
+        decimal_places=2,
+        db_column="valor"
+    )
+
+    source = models.CharField(
+        max_length=255,
+        db_column="fuente",
+        null=True,
+        blank=True
+    )
+
+    category = models.CharField(
+        max_length=100,
+        db_column="categoria",
+        null=True,
+        blank=True
+    )
+
+    payment_method = models.CharField(
+        max_length=100,
+        db_column="metododepago",
+        null=True,
+        blank=True
+    )
+
+    date = models.DateField(
+        db_column="fecha"
+    )
+
+    reference = models.CharField(
+        max_length=255,
+        db_column="referencia",
+        null=True,
+        blank=True
+    )
+
+    financial_planning_id = models.IntegerField(
+        db_column="idplanificacionfinanciera",
+        null=True,
+        blank=True
+    )
+
+    accounting_account_id = models.IntegerField(
+        db_column="cuentacontable_id",
+        null=True,
+        blank=True
+    )
+
+    user_id = models.IntegerField(
+        db_column="user_id"
+    )
+
     class Meta:
-        db_table = "ingresos"
-        managed = False
+        db_table = "ingreso"   # 👈 nombre exacto de la tabla
+        managed = False        # 👈 NO deja que Django la toque
+
+    def __str__(self):
+        return f"{self.source} - {self.value}"
--- apps/incomes/views.py
+++ apps/incomes/views.py
@@ -1,7 +1,76 @@
 from rest_framework import viewsets
 from .models import Income
 from .serializers import IncomeSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 class IncomeViewSet( viewsets.ModelViewSet ):
     queryset = Income.objects.all()
     serializer_class = IncomeSerializer
+
+    def  get_queryset(self):
+        queryset = Income.objects.all()
+
+        userId = self.request.query_params.get('user_id')
+        accountId =  self.request.query_params.get('account_id')
+
+        if userId:
+            queryset = queryset.filter(user_id=userId)
+
+        if accountId:
+            queryset = queryset.filter(accounting_account_id=accountId)
+
+        print( queryset )
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.queryset()
+
+        if not queryset.exists():
+            return Response(
+                {"message": "there is no results"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['put'], url_path='update-by-user-account')
+    def update_by_user_account(self, request):
+        user_id = request.data.get('user_id')
+        account_id = request.data.get('account_id')
+
+        try:
+            income = Income.objects.get(
+                user_id=user_id,
+                accounting_account_id=account_id
+            )
+        except Income.DoesNotExist:
+            return Response({"error": "No encontrado"}, status=404)
+
+        serializer = self.get_serializer(income, data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        serializer.save()
+
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['delete'], url_path='delete-by-user-account')
+    def delete_by_user_account(self, request):
+        user_id = request.query_params.get('user_id')
+        account_id = request.query_params.get('account_id')
+
+        try:
+            income = Income.objects.get(
+                user_id=user_id,
+                accounting_account_id=account_id
+            )
+        except Income.DoesNotExist:
+            return Response({"error": "No encontrado"}, status=404)
+
+        income.delete()
+        return Response(status=204)
